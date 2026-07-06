1b4e95bad22ce5289b4caf689cf984afd671f7a1
luczeng <luc.zeng@hotmail.fr>
2026-07-06 16:05:58 +0200

    Update tools/annotator/main_window.py

--- tools/annotator/main_window.py
+++ tools/annotator/main_window.py
@@ -181,6 +181,7 @@ class MainWindow(QMainWindow):
         toolbar.addWidget(self._next_btn)
         toolbar.addWidget(self._save_btn)
         toolbar.addWidget(self._delete_btn)
+        toolbar.addWidget(self._rename_btn)
         toolbar.addWidget(QLabel("🔊"))
         toolbar.addWidget(self._volume_slider)
         toolbar.addWidget(self._info_label)
