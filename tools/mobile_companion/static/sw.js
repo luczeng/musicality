90270cf61c8a17fe10fd0e5ecaade8cefdb2a19f
luczeng <luc.zeng@hotmail.fr>
2026-07-18 18:53:55 +0200

    Update tools/mobile_companion/static/sw.js

---
+++ tools/mobile_companion/static/sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = "musicality-mobile-companion-v1";
+const APP_SHELL = [
+  "/",
+  "/manifest.json",
+  "/static/icon-192.png",
+  "/static/icon-512.png",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((names) =>
+        Promise.all(
+          names
+            .filter((name) => name !== CACHE_NAME)
+            .map((name) => caches.delete(name))
+        )
+      )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
